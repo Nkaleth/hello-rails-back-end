7bb642c9a163d55eb17ca7d9b911344001fd860b
Nkaleth <nseguralu@gmail.com>
2023-06-06 16:38:31 -0500

    FIX rubocop offenses

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 class MessagesController < ApplicationController
   def random
-    @message = Message.order("RANDOM()").first
+    @message = Message.order('RANDOM()').first
     render json: @message
   end
 end

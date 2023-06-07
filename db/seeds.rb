bd45f4c84e094a4d31379395145fc1ede2bdf411
Nkaleth <nseguralu@gmail.com>
2023-06-06 19:14:38 -0500

    FIX rubocop offenses

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 # rubocop:disable Lint/UselessAssignment
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
